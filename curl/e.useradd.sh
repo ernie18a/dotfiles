1d516c51626e23e15e56c7f3b019f352860b692a
Test User <test@example.com>
2026-05-05 20:22:18 +0800

    PUSH

--- curl/e.useradd.sh
+++ curl/e.useradd.sh
@@ -1,11 +1,10 @@
 for user in o n; do
-	rm -rf /home/$user/.ssh /home/$user/.gemini/settings.json /home/$user/.gemini/GEMINI.md /home/$user/.gitconfig 
+	rm -rf /home/$user/.ssh /home/$user/.gemini/GEMINI.md /home/$user/.gitconfig 
     mkdir -p /home/$user/.gemini
     cp -rf /home/e/.G /home/$user/
     cp -rf /home/e/.ssh /home/$user/
-    ln -snf /home/$user/.G/dotfiles/home/settings.json /home/$user/.gemini/settings.json
     ln -snf /home/$user/.G/dotfiles/home/GEMINI.md /home/$user/.gemini/GEMINI.md
     ln -snf /home/$user/.G/dotfiles/home/.gitconfig /home/$user/.gitconfig
-	echo 'source /dev/stdin <<< "$(curl -Ls https://gitlab.com/ernie18a/dotfiles/-/raw/main/home/.bash_profile)"' > /home/$user/.bash_profile
+	echo 'source /dev/stdin <<< "$(curl -Ls https://raw.githubusercontent.com/ernie18a/dotfiles/main/home/.bash_profile)"' > /home/$user/.bash_profile
     chown -R $user:$user /home/$user
 done
