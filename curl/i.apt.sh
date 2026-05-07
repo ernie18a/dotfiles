ae33efd9edc37f8579278ed67842ececfe2b925e
Test User <test@example.com>
2026-05-07 08:55:13 +0800

    PUSH

--- curl/i.apt.sh
+++ curl/i.apt.sh
@@ -20,8 +20,11 @@
 case "$1" in
   wsl)
 	echo wsl
+	sudo install -d -m 0755 /etc/apt/keyrings
+    sudo curl -fsSL https://downloads.claude.ai/keys/claude-code.asc -o /etc/apt/keyrings/claude-code.asc
+	echo "deb [signed-by=/etc/apt/keyrings/claude-code.asc] https://downloads.claude.ai/claude-code/apt/stable stable main" | sudo tee /etc/apt/sources.list.d/claude-code.list
 	curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -  # nodejs
-	apt-get update ; apt-get install -yq python3-pip python3-venv dos2unix file jq software-properties-common tree unzip autossh sshpass nodejs # wsl
+	apt-get update ; apt-get install -yq claude-code  dos2unix file jq software-properties-common tree unzip autossh sshpass nodejs #  python3-pip python3-venv
 	npm install -g @google/gemini-cli # firebase-admin firebase-tools
 #	add-apt-repository ppa:git-core/ppa
 	apt-get update ; apt-get install -yq git
