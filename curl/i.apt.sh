a989cd00426b21412ff499a8563e74789be00306
Test User <test@example.com>
2026-05-24 05:46:03 +0800

    FPUSH

--- curl/i.apt.sh
+++ curl/i.apt.sh
@@ -12,7 +12,7 @@ install -d -m 0755 /etc/apt/keyrings
 curl -fsSL https://downloads.claude.ai/keys/claude-code.asc -o /etc/apt/keyrings/claude-code.asc
 echo "deb [signed-by=/etc/apt/keyrings/claude-code.asc] https://downloads.claude.ai/claude-code/apt/stable stable main" | tee /etc/apt/sources.list.d/claude-code.list
 curl -fsSL https://deb.nodesource.com/setup_current.x | bash -
-curl -fsSL https://raw.githubusercontent.com/ernie18a/dotfiles/refs/heads/main/curl/i.astral.uv.sh | bash
+curl -fsSL https://raw.githubusercontent.com/ernie18a/dotfiles/refs/heads/main/curl/i.uv.hermes.sh | bash
 apt-get update ; apt-get install -yq nodejs claude-code
 # npm install -g @google/gemini-cli
 curl -fsSL https://antigravity.google/cli/install.sh | bash
