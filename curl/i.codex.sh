cb77490576a2b7c06300f03f6c797b1f5d4001c7
Test User <test@example.com>
2026-05-26 15:52:27 +0800

    FPUSH

--- curl/i.codex.sh
+++ curl/i.codex.sh
@@ -5,4 +5,5 @@ mv /root/.codex/packages/standalone/current/bin/codex /bin/codex
 for user in e o n; do
   mkdir -p /home/$user/.codex
   ln -sf /home/$user/.G/dotfiles/home/GEMINI.md /home/$user/.codex/AGENTS.md
+  chown -R $user:$user /home/$user
 done
