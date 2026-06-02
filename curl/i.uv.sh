9c17b620ad83d29f37963c5d4ded842c1711255e
ernie18a <47798618+ernie18a@users.noreply.github.com>
2026-06-02 08:17:51 +0800

    Update i.uv.sh

--- curl/i.uv.sh
+++ curl/i.uv.sh
@@ -1,1 +1,2 @@
-curl -LsSf https://astral.sh/uv/install.sh | UV_INSTALL_DIR=/usr/local/bin sh
+# curl -LsSf https://astral.sh/uv/install.sh | UV_INSTALL_DIR=/usr/local/bin sh
+curl -LsSf https://astral.sh/uv/install.sh | sh
