1f94bd2a7c2b312db60b0d0a6db0182c10f829fc
Test User <test@example.com>
2026-06-21 09:56:53 +0800

    FPUSH

--- curl/e.skill.sh
+++ curl/e.skill.sh
@@ -4,13 +4,20 @@ for user in e o n; do
       sudo mkdir -p /home/$user/.hermes
       sudo mkdir -p /home/$user/.codex
 
+      sudo rm -f /home/$user/.codex/AGENTS.md
+      sudo rm -f /home/$user/.gemini/GEMINI.md
+      sudo rm -f /home/$user/.hermes/SOUL.md
+      sudo rm -f /home/$user/.claude/CLAUDE.md
+
       sudo ln -snf /home/$user/.G/dotfiles/skills /home/$user/.gemini/skills
       sudo ln -snf /home/$user/.G/dotfiles/skills /home/$user/.claude/skills
       sudo ln -snf /home/$user/.G/dotfiles/skills /home/$user/.hermes/skills
       sudo ln -snf /home/$user/.G/dotfiles/skills /home/$user/.codex/skills
 
-      sudo chown -h $user:$user /home/$user/.gemini/skills
-      sudo chown -h $user:$user /home/$user/.claude/skills
-      sudo chown -h $user:$user /home/$user/.hermes/skills
-      sudo chown -h $user:$user /home/$user/.codex/skills
+      sudo ln -snf /home/$user/.G/dotfiles/home/GEMINI.md /home/$user/.codex/AGENTS.md
+      sudo ln -snf /home/$user/.G/dotfiles/home/GEMINI.md /home/$user/.gemini/GEMINI.md
+      sudo ln -snf /home/$user/.G/dotfiles/home/GEMINI.md /home/$user/.hermes/SOUL.md
+      sudo ln -snf /home/$user/.G/dotfiles/home/GEMINI.md /home/$user/.claude/CLAUDE.md
+
+      sudo chown -R $user:$user /home/$user
   done
