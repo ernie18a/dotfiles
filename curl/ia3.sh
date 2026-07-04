0526bf9d759f8975157a1ec3d96ce1671cd7e80b
Test User <test@example.com>
2026-07-04 14:05:04 +0800

    PUSH

--- curl/ia3.sh
+++ curl/ia3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-ia3_wsl() {
-	:
-}
-
-ia3_pico() {
-	:
-}
+case "$1" in
+  wsl)
+#	cat /home/e/.G/dotfiles/home/.hyper.js > $WIN_USER_DIR/AppData/Roaming/Hyper/.hyper.js
+    ;;
+  oci)
+    # Replace this comment with the actual commands to be executed for Ampere
+    ;;
+esac
