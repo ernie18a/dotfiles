8c37791aa70451ec2ede6857e06c978485afa74f
Test User <test@example.com>
2026-07-04 14:15:49 +0800

    PUSH

--- curl/ia3.sh
+++ curl/ia3.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 case "$1" in
+# default zone
+echo a
   wsl)
+# wsl zone
 #	cat /home/e/.G/dotfiles/home/.hyper.js > $WIN_USER_DIR/AppData/Roaming/Hyper/.hyper.js
     ;;
-  oci)
+  vm)
+# vm zone
     # Replace this comment with the actual commands to be executed for Ampere
     ;;
 esac
