89b4433e52f359470e8376821c9eb1a4f9115bf5
Test User <test@example.com>
2026-07-04 11:11:57 +0800

    PUSH

--- curl/i.uv.sh
+++ curl/i.uv.sh
@@ -1,3 +1,1 @@
-# curl -LsSf https://astral.sh/uv/install.sh | UV_INSTALL_DIR=/usr/local/bin sh
 curl -LsSf https://astral.sh/uv/install.sh | sh
-install -m 0755 "$HOME/.local/bin/uv" "$HOME/.local/bin/uvx" /usr/local/bin/
