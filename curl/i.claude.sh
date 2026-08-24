e8835d72fc884a84f97bb8e2c3ae133e1644ba2a
Test User <test@example.com>
2026-08-24 20:45:44 +0800

    PUSH

--- curl/i.claude.sh
+++ curl/i.claude.sh
@@ -1,3 +1,4 @@
 install -d -m 0755 /etc/apt/keyrings
 curl -fsSL https://downloads.claude.ai/keys/claude-code.asc -o /etc/apt/keyrings/claude-code.asc
 echo "deb [signed-by=/etc/apt/keyrings/claude-code.asc] https://downloads.claude.ai/claude-code/apt/stable stable main" | tee /etc/apt/sources.list.d/claude-code.list
+sudo apt update ; sudo apt install -y claude-code
